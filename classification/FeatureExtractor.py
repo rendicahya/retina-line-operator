18e433ea4ae5d3880f06680f493a1bc1c7014f0c
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-05-15 15:52:33 +0700

    Refactoring

--- classification/FeatureExtractor.py
+++ classification/FeatureExtractor.py
@@ -6,8 +6,8 @@ from util.data_util import normalize
 
 
 class FeatureExtractor:
-    select_fg_idx = None
-    select_bg_idx = None
+    selected_fg_idx = None
+    selected_bg_idx = None
 
     def __init__(self, image, mask, path, size, ground_truth=None, n_features=None):
         self.image = image
@@ -18,27 +18,27 @@ class FeatureExtractor:
         if ground_truth is not None and n_features is not None:
             all_fg_idx = np.argwhere(ground_truth == 255)
             rand_fg_idx = np.random.choice(all_fg_idx.shape[0], n_features, False)
-            select_fg_idx = all_fg_idx[rand_fg_idx]
+            selected_fg_idx = all_fg_idx[rand_fg_idx]
 
             all_bg_idx = np.argwhere(ground_truth == 0)
             rand_bg_idx = np.random.choice(all_bg_idx.shape[0], n_features, False)
-            select_bg_idx = all_bg_idx[rand_bg_idx]
+            selected_bg_idx = all_bg_idx[rand_bg_idx]
 
-            self.select_fg_idx = tuple(select_fg_idx.T)
-            self.select_bg_idx = tuple(select_bg_idx.T)
+            self.selected_fg_idx = tuple(selected_fg_idx.T)
+            self.selected_bg_idx = tuple(selected_bg_idx.T)
 
     def get_pixel_feat(self):
         fov_data = self.image[self.mask == 255]
         norm_fov_data = normalize(fov_data).ravel()
 
-        if self.select_fg_idx is None or self.select_bg_idx is None:
+        if self.selected_fg_idx is None or self.selected_bg_idx is None:
             return norm_fov_data
         else:
             norm_image = np.zeros(self.mask.shape, np.float64)
             norm_image[self.mask == 255] = norm_fov_data
 
-            fg_feat = norm_image[self.select_fg_idx]
-            bg_feat = norm_image[self.select_bg_idx]
+            fg_feat = norm_image[self.selected_fg_idx]
+            bg_feat = norm_image[self.selected_bg_idx]
 
             return fg_feat, bg_feat
 
@@ -46,13 +46,13 @@ class FeatureExtractor:
         kernels = [(i, i) for i in range(3, 13, 2)]
         filtered_images = [cv2.GaussianBlur(self.image, kernel, 0) for kernel in kernels]
 
-        if self.select_fg_idx is None or self.select_bg_idx is None:
+        if self.selected_fg_idx is None or self.selected_bg_idx is None:
             return np.dstack(filtered_images)
         else:
-            fg_feat = [image[self.select_fg_idx] for image in filtered_images]
+            fg_feat = [image[self.selected_fg_idx] for image in filtered_images]
             fg_feat = np.column_stack(fg_feat)
 
-            bg_feat = [image[self.select_bg_idx] for image in filtered_images]
+            bg_feat = [image[self.selected_bg_idx] for image in filtered_images]
             bg_feat = np.column_stack(bg_feat)
 
             return fg_feat, bg_feat
@@ -62,13 +62,13 @@ class FeatureExtractor:
         kernels = [cv2.getGaborKernel((3, 3), 3, orientation, 5, .5) for orientation in orientations]
         filtered_images = [cv2.filter2D(self.image, -1, kernel) for kernel in kernels]
 
-        if self.select_fg_idx is None or self.select_bg_idx is None:
+        if self.selected_fg_idx is None or self.selected_bg_idx is None:
             return np.dstack(filtered_images)
         else:
-            fg_feat = [image[self.select_fg_idx] for image in filtered_images]
+            fg_feat = [image[self.selected_fg_idx] for image in filtered_images]
             fg_feat = np.column_stack(fg_feat)
 
-            bg_feat = [image[self.select_bg_idx] for image in filtered_images]
+            bg_feat = [image[self.selected_bg_idx] for image in filtered_images]
             bg_feat = np.column_stack(bg_feat)
 
             features = np.vstack((fg_feat, bg_feat))
@@ -82,14 +82,14 @@ class FeatureExtractor:
         fov_data = line_str[self.mask == 255]
         norm_fov_data = normalize(fov_data).ravel()
 
-        if self.select_fg_idx is None or self.select_bg_idx is None:
+        if self.selected_fg_idx is None or self.selected_bg_idx is None:
             return norm_fov_data
         else:
             norm_image = np.zeros(self.mask.shape, np.float64)
             norm_image[self.mask == 255] = norm_fov_data
 
-            fg_feat = norm_image[self.select_fg_idx]
-            bg_feat = norm_image[self.select_bg_idx]
+            fg_feat = norm_image[self.selected_fg_idx]
+            bg_feat = norm_image[self.selected_bg_idx]
 
             return fg_feat, bg_feat
 
@@ -98,13 +98,13 @@ class FeatureExtractor:
         fov_data = line_str[self.mask == 255]
         norm_fov_data = normalize(fov_data).ravel()
 
-        if self.select_fg_idx is None or self.select_bg_idx is None:
+        if self.selected_fg_idx is None or self.selected_bg_idx is None:
             return norm_fov_data
         else:
             norm_image = np.zeros(self.mask.shape, np.float64)
             norm_image[self.mask == 255] = norm_fov_data
 
-            fg_feat = norm_image[self.select_fg_idx]
-            bg_feat = norm_image[self.select_bg_idx]
+            fg_feat = norm_image[self.selected_fg_idx]
+            bg_feat = norm_image[self.selected_bg_idx]
 
             return fg_feat, bg_feat
