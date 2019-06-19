5a769458fec4e2f93aa660396b5ed1077d175b21
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-06-19 22:05:03 +0700

    It's late

--- classification/line_str_main.py
+++ classification/line_str_main.py
@@ -4,35 +4,14 @@ from dataset import DriveDatasetLoader
 from dataset.DriveDatasetLoader import DriveDatasetLoader
 from methods.single_line_opr import cached_line, subtract
 from methods.window_average import cached_integral
-from util.image_util import find_best_threshold
-from util.time import Time
-from util.print_color import *
-from util.image_util import normalize_masked
 from util.data_util import accuracy
+from util.image_util import find_thresh_one
+from util.image_util import normalize_masked
+from util.print_color import *
+from util.time import Time
 
 
-def threshold_all(thresh):
-    accuracies = []
-
-    for path, img, mask, ground in DriveDatasetLoader('D:/Datasets/DRIVE', 10).load_testing():
-        img = 255 - img[:, :, 1]
-        size = 15
-        time = Time()
-
-        time.start(path)
-        window_avg = cached_integral(path, img, mask, size)
-        line_img = cached_line(path, img, mask, size)
-        single_img = subtract(line_img, window_avg, mask)
-        single_img = normalize_masked(single_img, mask)
-        bin = cv2.threshold(single_img, thresh, 255, cv2.THRESH_BINARY)[1]
-        acc = accuracy(bin, ground)
-        accuracies.append(acc)
-        time.finish()
-
-    print(accuracies)
-
-
-def find_best():
+def find_best_each():
     for img_id in range(1, 5):
         path, img, mask, ground = DriveDatasetLoader('D:/Datasets/DRIVE', 10).load_testing_one(img_id)
         img = 255 - img[:, :, 1]
@@ -44,8 +23,7 @@ def find_best():
         line_img = cached_line(path, img, mask, size)
         single_img = subtract(line_img, window_avg, mask)
         single_img = normalize_masked(single_img, mask)
-        best_thresh, best_img = find_best_threshold(single_img, ground, mask)
-        acc = accuracy(best_img, ground)
+        best_thresh, best_img, best_acc = find_thresh_one(single_img, ground, mask)
         time.finish()
 
         green(f'Best threshold: {best_thresh}')
--- util/image_util.py
+++ util/image_util.py
@@ -1,6 +1,10 @@
 import cv2
+import numpy as np
 
+from methods.single_line_opr import cached_line, subtract
+from methods.window_average import cached_integral
 from util.data_util import accuracy
+from util.time import Time
 
 
 def normalize(image):
@@ -11,7 +15,7 @@ def normalize_masked(image, mask):
     return cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U, mask)
 
 
-def find_best_threshold(image, ground, mask):
+def find_thresh_one(image, ground, mask):
     best_acc = 0
     best_thresh = 0
     best_image = None
@@ -27,4 +31,36 @@ def find_best_threshold(image, ground, mask):
             best_thresh = thresh
             best_image = bin
 
-    return best_thresh, best_image
+    return best_thresh, best_image, best_acc
+
+
+def find_thresh_all(dataset):
+    best_acc = 0
+    best_thresh = 0
+
+    for thresh in range(1, 255):
+        acc_list = []
+
+        for path, img, mask, ground in dataset:
+            img = 255 - img[:, :, 1]
+            size = 15
+            time = Time()
+
+            time.start(path)
+            window_avg = cached_integral(path, img, mask, size)
+            line_img = cached_line(path, img, mask, size)
+            single_img = subtract(line_img, window_avg, mask)
+            single_img = normalize_masked(single_img, mask)
+            bin = cv2.threshold(single_img, thresh, 255, cv2.THRESH_BINARY)[1]
+            acc = accuracy(bin, ground)
+
+            acc_list.append(acc)
+            time.finish()
+
+        avg = np.average(acc_list)
+
+        if avg > best_acc:
+            best_acc = avg
+            best_thresh = thresh
+
+    return best_thresh, best_acc
