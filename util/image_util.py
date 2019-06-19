6ba0fe31ba77977f044bceceaf3862bcd489aa66
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-06-19 16:30:33 +0700

    Find best thresh

--- util/image_util.py
+++ util/image_util.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 from util.data_util import accuracy
 
@@ -12,26 +11,7 @@ def normalize_masked(image, mask):
     return cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U, mask)
 
 
-def find_best_threshold(image, mask, ground_truth):
-    best_correct = 0
-    best_threshold = 0
-    best_image = None
-
-    for t in range(1, 255):
-        thresh, bin = cv2.threshold(image, t, 255, cv2.THRESH_BINARY)
-        bin_fov = bin[mask == 255]
-        ground_fov = ground_truth[mask == 255]
-        correct = np.sum(bin_fov == ground_fov)
-
-        if correct > best_correct:
-            best_correct = correct
-            best_threshold = thresh
-            best_image = bin
-
-    return best_threshold, best_image
-
-
-def find_best_threshold2(image, mask, ground):
+def find_best_threshold(image, mask, ground):
     best_acc = 0
     best_thresh = 0
     best_image = None
