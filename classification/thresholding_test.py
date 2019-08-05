bd62e6559220657a4bef4da275c1910c3d662335
Randy Cahya Wihandika <rendicahya@gmail.com>
2019-08-05 13:05:45 +0700

    Moving to test_util.py

--- classification/thresholding_test.py
+++ classification/thresholding_test.py
@@ -1,107 +1,10 @@
-import cv2
-import numpy as np
-
 from dataset.DriveDatasetLoader import DriveDatasetLoader
 from methods.multi_line_opr import cached_multi_norm
-from methods.optic_disk import cached_disk_norm
-from methods.proposed import proposed_norm
 from methods.single_line_opr import cached_single_norm
-from util.data_util import accuracy
-from util.image_util import find_best_thresh
 from util.print_color import *
-from util.test_util import find_best_acc, get_accuracy, find_best_acc_disk
+from util.test_util import find_best_acc, get_accuracy, find_best_acc_disk, find_best_acc_proposed, get_accuracy_disk, \
+    get_accuracy_proposed
 from util.timer import Timer
-
-
-def find_best_acc_proposed(op, thresh, data):
-    best_acc = 0
-    best_thresh = 0
-    size = 15
-
-    for proposed_thresh in range(1, 255):
-        acc_list = []
-
-        print(proposed_thresh)
-
-        for path, img, mask, ground in data:
-            img = 255 - img[:, :, 1]
-            line_str = op(path, img, mask, size)
-            bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
-            min_window = proposed_norm(path, img, mask, size)
-            min_window = 255 - cv2.threshold(min_window, proposed_thresh, 255, cv2.THRESH_BINARY)[1]
-            min_window[mask == 0] = 0
-            bin[min_window == 255] = 0
-            bin_fov = bin[mask == 255]
-            ground_fov = ground[mask == 255]
-            acc = accuracy(bin_fov, ground_fov)
-
-            acc_list.append(acc)
-
-        avg = np.mean(acc_list)
-
-        if avg > best_acc:
-            best_acc = avg
-            best_thresh = thresh
-
-    return best_thresh, best_acc
-
-
-def get_accuracy_disk(op, data, thresh, disk_thresh):
-    size = 15
-    acc_list = []
-
-    for path, img, mask, ground in data:
-        img = 255 - img[:, :, 1]
-        line_str = op(path, img, mask, size)
-        bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
-
-        disk = cached_disk_norm(path, img, mask, size)
-        disk = cv2.threshold(disk, disk_thresh, 255, cv2.THRESH_BINARY)[1]
-        disk = cv2.erode(disk, np.ones((3, 3), np.uint8), iterations=1)
-        bin[disk == 255] = 0
-        bin_fov = bin[mask == 255]
-        ground_fov = ground[mask == 255]
-        acc = accuracy(bin_fov, ground_fov)
-
-        acc_list.append(acc)
-
-    return np.mean(acc_list)
-
-
-def get_accuracy_proposed(op, data, thresh, proposed_thresh):
-    size = 15
-    acc_list = []
-
-    for path, img, mask, ground in data:
-        img = 255 - img[:, :, 1]
-        line_str = op(path, img, mask, size)
-        bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
-        min_window = proposed_norm(path, img, mask, size)
-        min_window = 255 - cv2.threshold(min_window, proposed_thresh, 255, cv2.THRESH_BINARY)[1]
-        min_window[mask == 0] = 0
-        bin[min_window == 255] = 0
-        bin_fov = bin[mask == 255]
-        ground_fov = ground[mask == 255]
-        acc = accuracy(bin_fov, ground_fov)
-
-        acc_list.append(acc)
-
-    return np.mean(acc_list)
-
-
-def find_best_acc_avg_each(op, data):
-    acc_list = []
-    size = 15
-
-    for path, img, mask, ground in data:
-        img = 255 - img[:, :, 1]
-        line_str = op(path, img, mask, size)
-        acc = find_best_thresh(line_str, ground, mask)[2]
-
-        acc_list.append(acc)
-
-    print(np.mean(acc_list))
-    print(np.max(acc_list))
 
 
 def test_line():
--- util/test_util.py
+++ util/test_util.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
+from methods.optic_disk import cached_disk_norm
+from methods.proposed import proposed_norm
 from util.data_util import auc_score, accuracy
+from util.image_util import find_best_thresh
 
 
 def calc_auc(data, op):
@@ -17,7 +20,7 @@ def calc_auc(data, op):
     return np.mean(auc_list)
 
 
-def find_best_acc_train_data(op, data):
+def find_best_acc(op, data):
     best_acc = 0
     best_thresh = 0
     size = 15
@@ -42,3 +45,143 @@ def find_best_acc_train_data(op, data):
             best_thresh = thresh
 
     return best_thresh, best_acc
+
+
+def get_accuracy(op, data, thresh):
+    size = 15
+    acc_list = []
+
+    for path, img, mask, ground in data:
+        img = 255 - img[:, :, 1]
+        line_str = op(path, img, mask, size)
+        bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
+        bin_fov = bin[mask == 255]
+        ground_fov = ground[mask == 255]
+        acc = accuracy(bin_fov, ground_fov)
+
+        acc_list.append(acc)
+
+    return np.mean(acc_list)
+
+
+def find_best_acc_disk(op, thresh, data):
+    best_acc = 0
+    best_thresh = 0
+    size = 15
+
+    for disk_thresh in range(1, 255):
+        acc_list = []
+
+        for path, img, mask, ground in data:
+            img = 255 - img[:, :, 1]
+            line_str = op(path, img, mask, size)
+            bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
+
+            disk = cached_disk_norm(path, img, mask, size)
+            disk = cv2.threshold(disk, disk_thresh, 255, cv2.THRESH_BINARY)[1]
+            disk = cv2.erode(disk, np.ones((3, 3), np.uint8), iterations=1)
+            bin[disk == 255] = 0
+            bin_fov = bin[mask == 255]
+            ground_fov = ground[mask == 255]
+            acc = accuracy(bin_fov, ground_fov)
+
+            acc_list.append(acc)
+
+        avg = np.mean(acc_list)
+
+        if avg > best_acc:
+            best_acc = avg
+            best_thresh = thresh
+
+    return best_thresh, best_acc
+
+
+def find_best_acc_proposed(op, thresh, data):
+    best_acc = 0
+    best_thresh = 0
+    size = 15
+
+    for proposed_thresh in range(1, 255):
+        acc_list = []
+
+        print(proposed_thresh)
+
+        for path, img, mask, ground in data:
+            img = 255 - img[:, :, 1]
+            line_str = op(path, img, mask, size)
+            bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
+            min_window = proposed_norm(path, img, mask, size)
+            min_window = 255 - cv2.threshold(min_window, proposed_thresh, 255, cv2.THRESH_BINARY)[1]
+            min_window[mask == 0] = 0
+            bin[min_window == 255] = 0
+            bin_fov = bin[mask == 255]
+            ground_fov = ground[mask == 255]
+            acc = accuracy(bin_fov, ground_fov)
+
+            acc_list.append(acc)
+
+        avg = np.mean(acc_list)
+
+        if avg > best_acc:
+            best_acc = avg
+            best_thresh = thresh
+
+    return best_thresh, best_acc
+
+
+def get_accuracy_disk(op, data, thresh, disk_thresh):
+    size = 15
+    acc_list = []
+
+    for path, img, mask, ground in data:
+        img = 255 - img[:, :, 1]
+        line_str = op(path, img, mask, size)
+        bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
+
+        disk = cached_disk_norm(path, img, mask, size)
+        disk = cv2.threshold(disk, disk_thresh, 255, cv2.THRESH_BINARY)[1]
+        disk = cv2.erode(disk, np.ones((3, 3), np.uint8), iterations=1)
+        bin[disk == 255] = 0
+        bin_fov = bin[mask == 255]
+        ground_fov = ground[mask == 255]
+        acc = accuracy(bin_fov, ground_fov)
+
+        acc_list.append(acc)
+
+    return np.mean(acc_list)
+
+
+def get_accuracy_proposed(op, data, thresh, proposed_thresh):
+    size = 15
+    acc_list = []
+
+    for path, img, mask, ground in data:
+        img = 255 - img[:, :, 1]
+        line_str = op(path, img, mask, size)
+        bin = cv2.threshold(line_str, thresh, 255, cv2.THRESH_BINARY)[1]
+        min_window = proposed_norm(path, img, mask, size)
+        min_window = 255 - cv2.threshold(min_window, proposed_thresh, 255, cv2.THRESH_BINARY)[1]
+        min_window[mask == 0] = 0
+        bin[min_window == 255] = 0
+        bin_fov = bin[mask == 255]
+        ground_fov = ground[mask == 255]
+        acc = accuracy(bin_fov, ground_fov)
+
+        acc_list.append(acc)
+
+    return np.mean(acc_list)
+
+
+def find_best_acc_avg_each(op, data):
+    acc_list = []
+    size = 15
+
+    for path, img, mask, ground in data:
+        img = 255 - img[:, :, 1]
+        line_str = op(path, img, mask, size)
+        acc = find_best_thresh(line_str, ground, mask)[2]
+
+        acc_list.append(acc)
+
+    print(np.mean(acc_list))
+    print(np.max(acc_list))
