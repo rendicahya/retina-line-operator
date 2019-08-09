d49b26b8e3e52b70546a2f609ef6a09b2a6ae542
rendicahya <rendicahya@gmail.com>
2019-08-09 19:48:32 +0700

    Refactoring

--- test/thresholding_test.py
+++ test/thresholding_test.py
@@ -1,6 +1,6 @@
 from dataset.DriveDatasetLoader import DriveDatasetLoader
 from methods.multi_line_opr import cached_multi_norm, cached_multi
-from methods.single_line_opr import cached_single, cached_single_norm
+from methods.single_line_opr import cached_single
 from util.print_color import *
 from util.test.basic_test_util import basic_train, basic_get_acc, basic_calc_auc, basic_test_each
 from util.test.optic_test_util import optic_train, optic_test_each, optic_get_acc
@@ -21,19 +21,15 @@ def basic_training():
     green('basic_training')
     timer.start('Train')
     thresh, train_acc = basic_train(op, train_data, size)
-    # train_auc = basic_calc_auc(train_data, op, size)
     timer.stop()
 
     timer.start('Test')
     test_acc = basic_get_acc(op, test_data, thresh, size)
-    # test_auc = basic_calc_auc(test_data, op, size)
     timer.stop()
 
     green(f'Threshold: {thresh}')
     green(f'Train average accuracy: {train_acc}')
-    # green(f'Train average AUC: {train_auc}')
     green(f'Test average accuracy: {test_acc}')
-    # green(f'Test average AUC: {test_auc}')
 
     # cv2.imshow('Image', linestr)
     # cv2.waitKey(0)
@@ -41,11 +37,11 @@ def basic_training():
 
 
 def basic_each():
-    data = DriveDatasetLoader('D:/Datasets/DRIVE', 10).load_training()
-    # data = DriveDatasetLoader('D:/Datasets/DRIVE', 10).load_testing()
+    # data = DriveDatasetLoader('D:/Datasets/DRIVE', 10).load_training()
+    data = DriveDatasetLoader('D:/Datasets/DRIVE', 10).load_testing()
 
-    op = cached_single_norm
-    # op = cached_multi_norm
+    # op = cached_single_norm
+    op = cached_multi_norm
 
     size = 15
     timer = Timer()
@@ -135,4 +131,4 @@ def proposed_each():
 
 
 if __name__ == '__main__':
-    basic_training()
+    basic_each()
